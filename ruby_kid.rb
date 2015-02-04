75b1bf334cdbc05ec600899b284d2c89088159ee
jmister28 <jesselikesyoda@gmail.com>
2015-02-04 15:30:16 -0800

    commkt

--- ruby_kid.rb
+++ ruby_kid.rb
@@ -6,6 +6,15 @@ class Init
   def add(content)
      puts "adding #{content}"
 end
-@jesse = Init.new
-@jesse.init("Jesse", "Test")
-@jesse.add("hi")
+def var_import(variable_name)
+    puts variable_name
+    ll = variable_name
+    puts "Stored in variable: ll"
+    puts "make ll equal to @example_variable.var_import(variable_name) and call puts on ll"
+end
+end
+@nancy = Init.new
+@nancy.init("nancy", "test")
+@nancy.add("hi")
+hi = "we"
+@nancy.var_import(hi)
