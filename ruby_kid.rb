5691434118600f4271b4dbc8d13b85dd2e524ea6
Jesse <jesselikesyoda@gmail.com>
2015-02-04 16:36:13 -0800

    commit

--- ruby_kid.rb
+++ ruby_kid.rb
@@ -8,9 +8,8 @@ class Init
 end
 def var_import(variable_name)
     puts variable_name
-    ll = variable_name
     puts "Stored in variable: ll"
-    puts "make ll equal to @example_variable.var_import(variable_name) and call puts on ll"
+    
 end
 end
 
